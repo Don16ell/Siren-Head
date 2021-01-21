cf2ecca0d240e25a3bdab2eff53735b50d84ddd2
Don16ell <76142062+Don16ell@users.noreply.github.com>
2021-01-21 16:34:15 +0800

    Update SirenheadbossEntity.java

--- siren_head_1.15/src/main/java/net/mcreator/sirenhead/entity/SirenheadbossEntity.java
+++ siren_head_1.15/src/main/java/net/mcreator/sirenhead/entity/SirenheadbossEntity.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.common.DungeonHooks;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
@@ -27,6 +28,7 @@ import net.minecraft.item.Item;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.entity.ai.goal.SwimGoal;
 import net.minecraft.entity.ai.goal.RandomWalkingGoal;
@@ -35,19 +37,15 @@ import net.minecraft.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.entity.ai.goal.LookRandomlyGoal;
 import net.minecraft.entity.ai.goal.HurtByTargetGoal;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EntitySpawnPlacementRegistry;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.CreatureAttribute;
-import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.client.renderer.entity.model.EntityModel;
-import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.client.renderer.entity.MobRenderer;
-import net.minecraft.client.renderer.entity.IEntityRenderer;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.IRenderTypeBuffer;
 
 import net.mcreator.sirenhead.SirenHeadModElements;
 
@@ -64,8 +62,8 @@ public class SirenheadbossEntity extends SirenHeadModElements.ModElement {
 
 	@Override
 	public void initElements() {
-		entity = (EntityType.Builder.<CustomEntity>create(CustomEntity::new, EntityClassification.MONSTER).setShouldReceiveVelocityUpdates(true)
-				.setTrackingRange(64).setUpdateInterval(3).setCustomClientFactory(CustomEntity::new).size(0.6f, 1.8f)).build("sirenheadboss")
+		entity = (EntityType.Builder.<CustomEntity>create(CustomEntity::new, EntityClassification.AMBIENT).setShouldReceiveVelocityUpdates(true)
+				.setTrackingRange(64).setUpdateInterval(3).setCustomClientFactory(CustomEntity::new).size(0.6f, 2.5f)).build("sirenheadboss")
 						.setRegistryName("sirenheadboss");
 		elements.entities.add(() -> entity);
 		elements.items.add(() -> new SpawnEggItem(entity, -7905792, -7077888, new Item.Properties().group(ItemGroup.MISC))
@@ -75,23 +73,26 @@ public class SirenheadbossEntity extends SirenHeadModElements.ModElement {
 	@Override
 	public void init(FMLCommonSetupEvent event) {
 		for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
-			biome.getSpawns(EntityClassification.MONSTER).add(new Biome.SpawnListEntry(entity, 4, 1, 2));
+			boolean biomeCriteria = false;
+			if (ForgeRegistries.BIOMES.getKey(biome).equals(new ResourceLocation("forest")))
+				biomeCriteria = true;
+			if (!biomeCriteria)
+				continue;
+			biome.getSpawns(EntityClassification.AMBIENT).add(new Biome.SpawnListEntry(entity, 1, 1, 2));
 		}
-		EntitySpawnPlacementRegistry.register(entity, EntitySpawnPlacementRegistry.PlacementType.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
-				MonsterEntity::canMonsterSpawn);
+		EntitySpawnPlacementRegistry.register(entity, EntitySpawnPlacementRegistry.PlacementType.NO_RESTRICTIONS,
+				Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, MobEntity::canSpawnOn);
+		DungeonHooks.addDungeonMob(entity, 180);
 	}
 
 	@SubscribeEvent
 	@OnlyIn(Dist.CLIENT)
 	public void registerModels(ModelRegistryEvent event) {
 		RenderingRegistry.registerEntityRenderingHandler(entity, renderManager -> {
-			return new MobRenderer(renderManager, new Modelsiren_head(), 0.5f) {
-				{
-					this.addLayer(new GlowingLayer<>(this));
-				}
+			return new MobRenderer(renderManager, new Modelsiren_head_boss(), 0.5f) {
 				@Override
 				public ResourceLocation getEntityTexture(Entity entity) {
-					return new ResourceLocation("siren_head:textures/siren_head_texture.png");
+					return new ResourceLocation("siren_head:textures/siren_head_boss.png");
 				}
 			};
 		});
@@ -118,10 +119,11 @@ public class SirenheadbossEntity extends SirenHeadModElements.ModElement {
 			this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.2, false));
 			this.targetSelector.addGoal(2, new NearestAttackableTargetGoal(this, PlayerEntity.class, true, true));
 			this.targetSelector.addGoal(3, new NearestAttackableTargetGoal(this, ServerPlayerEntity.class, true, true));
-			this.goalSelector.addGoal(4, new RandomWalkingGoal(this, 1));
-			this.targetSelector.addGoal(5, new HurtByTargetGoal(this).setCallsForHelp(this.getClass()));
-			this.goalSelector.addGoal(6, new LookRandomlyGoal(this));
-			this.goalSelector.addGoal(7, new SwimGoal(this));
+			this.targetSelector.addGoal(4, new NearestAttackableTargetGoal(this, AnimalEntity.class, true, true));
+			this.goalSelector.addGoal(5, new RandomWalkingGoal(this, 1));
+			this.targetSelector.addGoal(6, new HurtByTargetGoal(this).setCallsForHelp(this.getClass()));
+			this.goalSelector.addGoal(7, new LookRandomlyGoal(this));
+			this.goalSelector.addGoal(8, new SwimGoal(this));
 		}
 
 		@Override
@@ -161,12 +163,18 @@ public class SirenheadbossEntity extends SirenHeadModElements.ModElement {
 			if (this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED) != null)
 				this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.3);
 			if (this.getAttribute(SharedMonsterAttributes.MAX_HEALTH) != null)
-				this.getAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(500);
+				this.getAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(600);
 			if (this.getAttribute(SharedMonsterAttributes.ARMOR) != null)
-				this.getAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(55);
+				this.getAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(25);
 			if (this.getAttribute(SharedMonsterAttributes.ATTACK_DAMAGE) == null)
 				this.getAttributes().registerAttribute(SharedMonsterAttributes.ATTACK_DAMAGE);
-			this.getAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(40);
+			this.getAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(55);
+			if (this.getAttribute(SharedMonsterAttributes.KNOCKBACK_RESISTANCE) == null)
+				this.getAttributes().registerAttribute(SharedMonsterAttributes.KNOCKBACK_RESISTANCE);
+			this.getAttribute(SharedMonsterAttributes.KNOCKBACK_RESISTANCE).setBaseValue(2D);
+			if (this.getAttribute(SharedMonsterAttributes.ATTACK_KNOCKBACK) == null)
+				this.getAttributes().registerAttribute(SharedMonsterAttributes.ATTACK_KNOCKBACK);
+			this.getAttribute(SharedMonsterAttributes.ATTACK_KNOCKBACK).setBaseValue(2D);
 		}
 
 		@Override
@@ -193,65 +201,61 @@ public class SirenheadbossEntity extends SirenHeadModElements.ModElement {
 		}
 	}
 
-	@OnlyIn(Dist.CLIENT)
-	private static class GlowingLayer<T extends Entity, M extends EntityModel<T>> extends LayerRenderer<T, M> {
-		public GlowingLayer(IEntityRenderer<T, M> er) {
-			super(er);
-		}
-
-		public void render(MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn, T entitylivingbaseIn, float limbSwing,
-				float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
-			IVertexBuilder ivertexbuilder = bufferIn
-					.getBuffer(RenderType.getEyes(new ResourceLocation("siren_head:textures/siren_head_texture.png")));
-			this.getEntityModel().render(matrixStackIn, ivertexbuilder, 15728640, OverlayTexture.NO_OVERLAY, 1, 1, 1, 1);
-		}
-	}
-
-	// Made with Blockbench 3.7.4
+	// Made with Blockbench 3.7.5
 	// Exported for Minecraft version 1.15
 	// Paste this class into your mod and generate all required imports
-	public static class Modelsiren_head extends EntityModel<Entity> {
+	public static class Modelsiren_head_boss extends EntityModel<Entity> {
 		private final ModelRenderer head;
-		private final ModelRenderer Sirens;
-		private final ModelRenderer Sirens2;
+		private final ModelRenderer r_siren;
+		private final ModelRenderer l_siren;
 		private final ModelRenderer body;
-		private final ModelRenderer rightArm;
-		private final ModelRenderer leftArm;
-		private final ModelRenderer rightLeg;
-		private final ModelRenderer leftLeg;
-		public Modelsiren_head() {
+		private final ModelRenderer r_arm;
+		private final ModelRenderer l_arm;
+		private final ModelRenderer r_leg;
+		private final ModelRenderer l_leg;
+		public Modelsiren_head_boss() {
 			textureWidth = 128;
 			textureHeight = 128;
 			head = new ModelRenderer(this);
-			head.setRotationPoint(-2.0F, -20.0F, 2.0F);
-			head.setTextureOffset(64, 64).addBox(0.5F, -27.5F, -3.5F, 3.0F, 14.0F, 3.0F, -0.5F, false);
-			Sirens = new ModelRenderer(this);
-			Sirens.setRotationPoint(0.0F, -7.0F, 0.0F);
-			head.addChild(Sirens);
-			Sirens.setTextureOffset(60, 43).addBox(-2.5F, -17.5F, -4.0F, 4.0F, 4.0F, 4.0F, -0.5F, false);
-			Sirens.setTextureOffset(57, 9).addBox(-5.5F, -18.5F, -4.5F, 4.0F, 6.0F, 5.0F, -0.5F, false);
-			Sirens.setTextureOffset(40, 14).addBox(-9.5F, -19.5F, -5.0F, 5.0F, 8.0F, 6.0F, -0.5F, false);
-			Sirens2 = new ModelRenderer(this);
-			Sirens2.setRotationPoint(12.0F, -6.0F, 0.0F);
-			head.addChild(Sirens2);
-			Sirens2.setTextureOffset(48, 39).addBox(-9.5F, -18.5F, -4.0F, 4.0F, 4.0F, 4.0F, -0.5F, false);
-			Sirens2.setTextureOffset(48, 28).addBox(-6.5F, -19.5F, -4.5F, 4.0F, 6.0F, 5.0F, -0.5F, false);
-			Sirens2.setTextureOffset(40, 0).addBox(-3.5F, -20.5F, -5.0F, 5.0F, 8.0F, 6.0F, -0.5F, false);
+			head.setRotationPoint(0.0F, -26.0F, 0.0F);
+			head.setTextureOffset(0, 66).addBox(-2.0F, -13.0F, -2.0F, 4.0F, 13.0F, 4.0F, 0.0F, false);
+			r_siren = new ModelRenderer(this);
+			r_siren.setRotationPoint(0.0F, 45.0F, 0.0F);
+			head.addChild(r_siren);
+			r_siren.setTextureOffset(68, 49).addBox(-3.0F, -55.0F, -3.0F, 1.0F, 6.0F, 6.0F, 0.0F, false);
+			r_siren.setTextureOffset(68, 18).addBox(-4.0F, -56.0F, -3.0F, 1.0F, 8.0F, 6.0F, 0.0F, false);
+			r_siren.setTextureOffset(28, 71).addBox(-9.0F, -57.0F, -4.0F, 5.0F, 9.0F, 1.0F, 0.0F, false);
+			r_siren.setTextureOffset(16, 71).addBox(-9.0F, -57.0F, 3.0F, 5.0F, 9.0F, 1.0F, 0.0F, false);
+			r_siren.setTextureOffset(64, 9).addBox(-9.0F, -48.0F, -4.0F, 5.0F, 1.0F, 8.0F, 0.0F, false);
+			r_siren.setTextureOffset(64, 0).addBox(-9.0F, -57.0F, -4.0F, 5.0F, 1.0F, 8.0F, 0.0F, false);
+			l_siren = new ModelRenderer(this);
+			l_siren.setRotationPoint(13.0F, 45.0F, 0.0F);
+			head.addChild(l_siren);
+			l_siren.setTextureOffset(36, 24).addBox(-11.0F, -55.0F, -3.0F, 1.0F, 6.0F, 6.0F, 0.0F, false);
+			l_siren.setTextureOffset(68, 68).addBox(-10.0F, -56.0F, -3.0F, 1.0F, 8.0F, 6.0F, 0.0F, false);
+			l_siren.setTextureOffset(38, 60).addBox(-9.0F, -57.0F, -4.0F, 5.0F, 9.0F, 1.0F, 0.0F, false);
+			l_siren.setTextureOffset(0, 50).addBox(-9.0F, -57.0F, 3.0F, 5.0F, 9.0F, 1.0F, 0.0F, false);
+			l_siren.setTextureOffset(20, 62).addBox(-9.0F, -48.0F, -4.0F, 5.0F, 1.0F, 8.0F, 0.0F, false);
+			l_siren.setTextureOffset(60, 40).addBox(-9.0F, -57.0F, -4.0F, 5.0F, 1.0F, 8.0F, 0.0F, false);
 			body = new ModelRenderer(this);
-			body.setRotationPoint(2.0F, -20.0F, -2.0F);
-			body.setTextureOffset(0, 0).addBox(-8.0F, -14.0F, -2.0F, 12.0F, 26.0F, 8.0F, 0.0F, false);
-			rightArm = new ModelRenderer(this);
-			rightArm.setRotationPoint(-8.0F, -33.0F, 1.0F);
-			rightArm.setTextureOffset(48, 48).addBox(-2.0F, -1.0F, -2.0F, 4.0F, 40.0F, 4.0F, 0.0F, false);
-			leftArm = new ModelRenderer(this);
-			leftArm.setRotationPoint(8.0F, -33.0F, 1.0F);
-			leftArm.setTextureOffset(32, 34).addBox(-2.0F, -1.0F, -2.0F, 4.0F, 40.0F, 4.0F, 0.0F, false);
-			rightLeg = new ModelRenderer(this);
-			rightLeg.setRotationPoint(-3.0F, -8.0F, 1.0F);
-			rightLeg.setTextureOffset(16, 34).addBox(-2.0F, -4.0F, -2.0F, 4.0F, 40.0F, 4.0F, 0.0F, false);
-			leftLeg = new ModelRenderer(this);
-			leftLeg.setRotationPoint(3.0F, -8.0F, 1.0F);
-			leftLeg.setTextureOffset(0, 34).addBox(-2.0F, -4.0F, -2.0F, 4.0F, 40.0F, 4.0F, 0.0F, false);
+			body.setRotationPoint(0.0F, 24.0F, 0.0F);
+			body.setTextureOffset(0, 0).addBox(-4.0F, -50.0F, -2.0F, 8.0F, 22.0F, 4.0F, 0.0F, false);
+			r_arm = new ModelRenderer(this);
+			r_arm.setRotationPoint(-6.0F, -25.0F, -1.0F);
+			r_arm.setTextureOffset(52, 24).addBox(-2.0F, -1.0F, -1.0F, 4.0F, 20.0F, 4.0F, 0.0F, false);
+			r_arm.setTextureOffset(52, 52).addBox(-2.0F, 19.0F, -1.0F, 4.0F, 20.0F, 4.0F, 0.0F, false);
+			l_arm = new ModelRenderer(this);
+			l_arm.setRotationPoint(6.0F, -25.0F, -1.0F);
+			l_arm.setTextureOffset(48, 0).addBox(-2.0F, -1.0F, -1.0F, 4.0F, 20.0F, 4.0F, 0.0F, false);
+			l_arm.setTextureOffset(12, 46).addBox(-2.0F, 19.0F, -1.0F, 4.0F, 20.0F, 4.0F, 0.0F, false);
+			r_leg = new ModelRenderer(this);
+			r_leg.setRotationPoint(-2.0F, -4.0F, -1.5F);
+			r_leg.setTextureOffset(36, 36).addBox(-2.0F, 0.0F, -0.5F, 4.0F, 20.0F, 4.0F, 0.0F, false);
+			r_leg.setTextureOffset(32, 0).addBox(-2.0F, 20.0F, -0.5F, 4.0F, 20.0F, 4.0F, 0.0F, false);
+			l_leg = new ModelRenderer(this);
+			l_leg.setRotationPoint(2.0F, -4.0F, 0.0F);
+			l_leg.setTextureOffset(0, 26).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 20.0F, 4.0F, 0.0F, false);
+			l_leg.setTextureOffset(20, 22).addBox(-2.0F, 20.0F, -2.0F, 4.0F, 20.0F, 4.0F, 0.0F, false);
 		}
 
 		@Override
@@ -259,10 +263,10 @@ public class SirenheadbossEntity extends SirenHeadModElements.ModElement {
 				float alpha) {
 			head.render(matrixStack, buffer, packedLight, packedOverlay);
 			body.render(matrixStack, buffer, packedLight, packedOverlay);
-			rightArm.render(matrixStack, buffer, packedLight, packedOverlay);
-			leftArm.render(matrixStack, buffer, packedLight, packedOverlay);
-			rightLeg.render(matrixStack, buffer, packedLight, packedOverlay);
-			leftLeg.render(matrixStack, buffer, packedLight, packedOverlay);
+			r_arm.render(matrixStack, buffer, packedLight, packedOverlay);
+			l_arm.render(matrixStack, buffer, packedLight, packedOverlay);
+			r_leg.render(matrixStack, buffer, packedLight, packedOverlay);
+			l_leg.render(matrixStack, buffer, packedLight, packedOverlay);
 		}
 
 		public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
@@ -274,10 +278,10 @@ public class SirenheadbossEntity extends SirenHeadModElements.ModElement {
 		public void setRotationAngles(Entity e, float f, float f1, float f2, float f3, float f4) {
 			this.head.rotateAngleY = f3 / (180F / (float) Math.PI);
 			this.head.rotateAngleX = f4 / (180F / (float) Math.PI);
-			this.rightLeg.rotateAngleX = MathHelper.cos(f * 1.0F) * 1.0F * f1;
-			this.rightArm.rotateAngleX = MathHelper.cos(f * 0.6662F + (float) Math.PI) * f1;
-			this.leftArm.rotateAngleX = MathHelper.cos(f * 0.6662F) * f1;
-			this.leftLeg.rotateAngleX = MathHelper.cos(f * 1.0F) * -1.0F * f1;
+			this.r_leg.rotateAngleX = MathHelper.cos(f * 1.0F) * 1.0F * f1;
+			this.l_leg.rotateAngleX = MathHelper.cos(f * 1.0F) * -1.0F * f1;
+			this.l_arm.rotateAngleY = MathHelper.cos(f * 0.6662F) * f1;
+			this.r_arm.rotateAngleX = MathHelper.cos(f * 0.6662F + (float) Math.PI) * f1;
 		}
 	}
 }
